be08ba3b93575e145c7aae85cbbf21d9e878e5c6
Mert Vardar <mvard@dfds.com>
2022-08-19 21:57:25 +0300

    Request data from URL on load

--- LaunchesTests/RemoteLaunchLoaderTests.swift
+++ LaunchesTests/RemoteLaunchLoaderTests.swift
@@ -8,19 +8,34 @@
 import XCTest
 
 class RemoteLaunchLoader {
-
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared: HTTPClient = HTTPClient()
+
+    private init() {}
+
     var requestedURL: URL?
 }
 
 class RemoteLaunchLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteLaunchLoader()
 
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteLaunchLoader()
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 }
