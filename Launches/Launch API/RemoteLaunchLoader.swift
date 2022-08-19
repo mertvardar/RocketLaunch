ffa140eb3a1818670e0ee8e326938f18584a9bdb
Mert Vardar <mvard@dfds.com>
2022-08-20 00:24:35 +0300

    Delivers invalidData error on non 200 HTTP responses

--- Launches/Launch API/RemoteLaunchLoader.swift
+++ Launches/Launch API/RemoteLaunchLoader.swift
@@ -13,6 +13,7 @@ public final class RemoteLaunchLoader {
 
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
 
     public init(url: URL, client: HTTPClient) {
@@ -21,8 +22,12 @@ public final class RemoteLaunchLoader {
     }
 
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
