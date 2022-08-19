9b81bface9b4edf6b56d7b4ed816c2f2556edd9b
Mert Vardar <mvard@dfds.com>
2022-08-19 21:42:36 +0300

    Create the LaunchItem model

---
+++ Launches/LaunchItem.swift
@@ -0,0 +1,14 @@
+//
+//  LaunchItem.swift
+//  Launches
+//
+//  Created by Mert Vardar on 19.08.2022.
+//
+
+import Foundation
+
+struct LaunchItem {
+    let id: Int
+    let name: String
+    let date: String
+}
