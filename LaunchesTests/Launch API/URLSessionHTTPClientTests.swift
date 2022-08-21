394cd3062f5ed31e7b22a959bca2c2d5e36419fc
Mert Vardar <mvard@dfds.com>
2022-08-21 18:08:40 +0300

    Helper method used

--- LaunchesTests/Launch API/URLSessionHTTPClientTests.swift
+++ LaunchesTests/Launch API/URLSessionHTTPClientTests.swift
@@ -58,7 +58,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_getFromURL_failsOnRequestError() {
-        let requestError = NSError(domain: "any error", code: 1)
+        let requestError = anyNSError()
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError) as NSError?
 
         XCTAssertEqual(receivedError?.domain, requestError.domain)
