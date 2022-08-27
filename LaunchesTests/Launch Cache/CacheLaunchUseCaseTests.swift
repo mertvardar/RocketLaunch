357de3186a53f718419ba092da4db044dc25fa02
Mert Vardar <mvard@dfds.com>
2022-08-27 15:16:22 +0300

    Save command does not command cache insertion on deletion error

--- LaunchesTests/Launch Cache/CacheLaunchUseCaseTests.swift
+++ LaunchesTests/Launch Cache/CacheLaunchUseCaseTests.swift
@@ -22,9 +22,14 @@ class LocalLaunchLoader {
 
 class LaunchStore {
     var deleteCachedLaunchCallCount = 0
+    var insertCallCount = 0
 
     func deleteCachedLaunhes() {
         deleteCachedLaunchCallCount += 1
+    }
+
+    func completeDeletion(with error: Error, at index: Int = 0) {
+
     }
 }
 
@@ -44,6 +49,18 @@ class CacheLaunchUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCachedLaunchCallCount, 1)
     }
 
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let (sut, store) = makeSUT()
+        let items = [LaunchItem(id: 0, name: "Launch 1", date: "01012022"),
+                     LaunchItem(id: 1, name: "Launch 2", date: "02012022")]
+        let deletionError = anyNSError()
+
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #file,
@@ -57,5 +74,9 @@ class CacheLaunchUseCaseTests: XCTestCase {
 
     private func anyURL() -> URL {
         return URL(string: "http://any-url.com")!
+    }
+
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }
