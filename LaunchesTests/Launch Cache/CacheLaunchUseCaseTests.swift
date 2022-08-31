630ccae56355e71d19e0b4c4a944929a6b3f8986
Mert Vardar <mvard@dfds.com>
2022-09-01 00:11:10 +0300

    Extract LaunchStoreSpy

--- LaunchesTests/Launch Cache/CacheLaunchUseCaseTests.swift
+++ LaunchesTests/Launch Cache/CacheLaunchUseCaseTests.swift
@@ -141,46 +141,4 @@ class CacheLaunchUseCaseTests: XCTestCase {
     private func anyNSError() -> NSError {
         return NSError(domain: "any error", code: 0)
     }
-
-    private class LaunchStoreSpy: LaunchStore {
-        typealias DeletionCompletion = (Error?) -> Void
-        typealias InsertionCompletion = (Error?) -> Void
-
-        enum ReceivedMessage: Equatable {
-            case deleteCacheLaunch
-            case insertCacheLaunch([LocalLaunchItem], Date)
-        }
-        private(set) var receivedMessages = [ReceivedMessage]()
-
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-
-        func deleteCachedLaunches(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCacheLaunch)
-        }
-
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-
-        func insert(_ launchItems: [LocalLaunchItem],
-                    timestamp: Date,
-                    completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.insertCacheLaunch(launchItems, timestamp))
-        }
-
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
 }
--- LaunchesTests/Launch Cache/LoadLaunchFromCacheUseCaseTests.swift
+++ LaunchesTests/Launch Cache/LoadLaunchFromCacheUseCaseTests.swift
@@ -27,46 +27,4 @@ class LoadLaunchFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         return (sut, store)
     }
-
-    private class LaunchStoreSpy: LaunchStore {
-        typealias DeletionCompletion = (Error?) -> Void
-        typealias InsertionCompletion = (Error?) -> Void
-
-        enum ReceivedMessage: Equatable {
-            case deleteCacheLaunch
-            case insertCacheLaunch([LocalLaunchItem], Date)
-        }
-        private(set) var receivedMessages = [ReceivedMessage]()
-
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-
-        func deleteCachedLaunches(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCacheLaunch)
-        }
-
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-
-        func insert(_ launchItems: [LocalLaunchItem],
-                    timestamp: Date,
-                    completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.insertCacheLaunch(launchItems, timestamp))
-        }
-
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
 }
