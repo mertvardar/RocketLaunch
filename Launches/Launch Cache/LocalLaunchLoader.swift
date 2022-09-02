359bee8e2f1be4f0b6f59a87b93a06aa8ffb1bc4
Mert Vardar <mvard@dfds.com>
2022-09-02 23:20:40 +0300

    Load command delivers empty on seven days old cache

--- Launches/Launch Cache/LocalLaunchLoader.swift
+++ Launches/Launch Cache/LocalLaunchLoader.swift
@@ -33,16 +33,22 @@ public final class LocalLaunchLoader {
     }
 
     public func load(completion: @escaping (LoadResult?) -> Void) {
-        store.retrieve { result in
+        store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-            case let .found(launches, _):
+            case let .found(launches, timestamp) where self.validate(timestamp):
                 completion(.success(launches.toModels()))
-            case .empty:
+            case .found, .empty:
                 completion(.success([]))
             }
         }
+    }
+
+    private func validate(_ timestamp: Date) -> Bool {
+        let calendar = Calendar(identifier: .gregorian)
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        return currentDate() < maxCacheAge
     }
 
     private func cache(_ items: [LaunchItem], with completion: @escaping (SaveResult) -> Void) {
