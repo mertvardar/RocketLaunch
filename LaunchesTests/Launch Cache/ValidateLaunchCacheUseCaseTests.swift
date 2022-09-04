8052be9d416e5d953951c792a614b8121b9908a0
Mert Vardar <mvard@dfds.com>
2022-09-05 00:27:09 +0300

    LocalLaunchLoader does not message store upon creation (before validating the cached launches)

---
+++ LaunchesTests/Launch Cache/ValidateLaunchCacheUseCaseTests.swift
@@ -0,0 +1,30 @@
+//
+//  ValidateLaunchCacheUseCaseTests.swift
+//  LaunchesTests
+//
+//  Created by Mert Vardar on 5.09.2022.
+//
+
+import XCTest
+import Launches
+
+class ValidateLaunchCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #file,
+                         line: UInt = #line) -> (sut: LocalLaunchLoader, store: LaunchStoreSpy) {
+        let store = LaunchStoreSpy()
+        let sut = LocalLaunchLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
+}
