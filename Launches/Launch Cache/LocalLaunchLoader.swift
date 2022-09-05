0475dee0ccc95307e60a4bd74e589efed4a577a6
Mert Vardar <mvard@dfds.com>
2022-09-05 16:36:30 +0300

    Move LaunchCachePolicy its own file

--- Launches/Launch Cache/LocalLaunchLoader.swift
+++ Launches/Launch Cache/LocalLaunchLoader.swift
@@ -7,21 +7,6 @@
 
 import Foundation
 
-private final class LaunchCachePolicy {
-    private init() {}
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-
-    private static var maxCacheAgeInDays: Int {
-        return 7
-    }
-
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalLaunchLoader: LaunchLoader {
     private let store: LaunchStore
     private let currentDate: () -> Date
