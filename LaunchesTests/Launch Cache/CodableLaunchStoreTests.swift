b481b840d695b431f1fad846242b411d11870bb9
Mert Vardar <mvard@dfds.com>
2022-09-05 22:59:24 +0300

    Retrieving empty cache twice has no side effects

--- LaunchesTests/Launch Cache/CodableLaunchStoreTests.swift
+++ LaunchesTests/Launch Cache/CodableLaunchStoreTests.swift
@@ -34,4 +34,25 @@ class CodableLaunchStoreTests: XCTestCase {
 
         wait(for: [exp], timeout: 1.0)
     }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableLaunchStore()
+        let exp = expectation(description: "Wait for completion")
+
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+
+                default:
+                    XCTFail("Expected both empty result, got \(firstResult) and \(secondResult) instead")
+                }
+
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
 }
