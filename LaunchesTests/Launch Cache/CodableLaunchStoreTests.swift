687e905b50d822858ca2c898296cfef94d24e901
Mert Vardar <mvard@dfds.com>
2022-09-06 23:02:53 +0300

    Create 'LaunchStoreSpecs' that must be implemented by any 'LaunchStore' test implementation to guarantee it meets spec

--- LaunchesTests/Launch Cache/CodableLaunchStoreTests.swift
+++ LaunchesTests/Launch Cache/CodableLaunchStoreTests.swift
@@ -8,40 +8,8 @@
 import XCTest
 import Launches
 
-protocol LaunchStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
-
-    func test_insert_deliversNoErrorOnEmptyCache()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_overridesPreviouslyInsertedCacheValues()
-
-    func test_delete_deliversNoErrorOnEmptyCache()
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_deletion_emptiesPreviouslyInsertedCache()
-
-    func test_storeSideEffects_runSerially()
-}
-
-protocol FailableRetrieveLaunchSpecs {
-    func test_retrieve_deliversFailureOnRetrievalError()
-    func test_retrieve_deliversFailureTwiceOnRetrievalError()
-}
-
-protocol FailableInsertLaunchSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-}
-
-protocol FailableDeleteLaunchSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectsOnDeletionError()
-}
-
-class CodableLaunchStoreTests: XCTestCase {
+typealias FailableLaunchStore = FailableRetrieveLaunchSpecs & FailableInsertLaunchSpecs & FailableDeleteLaunchSpecs
+class CodableLaunchStoreTests: XCTestCase, FailableLaunchStore {
 
     override func setUp() {
         super.setUp()
