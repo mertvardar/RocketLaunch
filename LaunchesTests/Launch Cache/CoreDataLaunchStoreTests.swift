8a818a97cc806afe29625b91edc4aef6cbd7ddf8
Mert Vardar <mvard@dfds.com>
2022-10-04 21:27:31 +0300

    Add empty CoreData launch store specs

---
+++ LaunchesTests/Launch Cache/CoreDataLaunchStoreTests.swift
@@ -0,0 +1,58 @@
+//
+//  CoreDataLaunchStoreTests.swift
+//  LaunchesTests
+//
+//  Created by Mert Vardar on 4.10.2022.
+//
+
+import XCTest
+
+class CoreDataLaunchStoreTests: XCTestCase, LaunchStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+
+    }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+
+    }
+
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+
+    }
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+
+    }
+
+    func test_insert_deliversNoErrorOnEmptyCache() {
+
+    }
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+
+    }
+
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+
+    }
+
+    func test_delete_deliversNoErrorOnEmptyCache() {
+
+    }
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+
+    }
+
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
+    }
+
+    func test_delete_emptiesPreviouslyInsertedCache() {
+
+    }
+
+    func test_storeSideEffects_runSerially() {
+
+    }
+}
