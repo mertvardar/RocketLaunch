c6c30b84beee4a4fa24a40dcd5c0211c22bda528
Mert Vardar <mvard@dfds.com>
2022-10-05 21:16:15 +0300

    Add 'LaunchesCacheIntegrationTests' target to separate the potentially-slower cache integration tests from the test unit/isolated tests

---
+++ LaunchesCacheIntegrationTests/LaunchesCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  LaunchesCacheIntegrationTests.swift
+//  LaunchesCacheIntegrationTests
+//
+//  Created by Mert Vardar on 5.10.2022.
+//
+
+import XCTest
+
+class LaunchesCacheIntegrationTests: XCTestCase {
+    
+}
